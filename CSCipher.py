056dca4e34d5a3ca4dd3f1873f3cfc63a93cc730
stepa <stepan26062001@gmail.com>
2021-12-10 02:02:19 +0300

    generation subkeys - done

--- CSCipher.py
+++ CSCipher.py
@@ -21,33 +21,70 @@ class CSCipher:
         0x03c54b5a46a34465
     ]
     subkeys = []
-    Fx = [0xf, 0xd, 0xb, 0xb, 0x7, 0x5, 0x7, 0x7, 0xe, 0xd, 0xa, 0xb, 0xe, 0xd,	0xe, 0xf]
+    Fx = [0xf, 0xd, 0xb, 0xb, 0x7, 0x5, 0x7, 0x7, 0xe, 0xd, 0xa, 0xb, 0xe, 0xd, 0xe, 0xf]
     Gx = [0xa, 0x6, 0x0, 0x2, 0xb, 0xe, 0x1, 0x8, 0xd, 0x4, 0x5, 0x3, 0xf, 0xc, 0x7, 0x9]
 
     def __init__(self, file: str, decrypt: bool, key=None, ):
         self.key = key
         self.decrypt_mode = decrypt
+        buf = file.encode().hex()
+        if len(buf) % 16 != 0:
+            buf = self.fill_to_full(buf, len(buf) + len(buf) % 16)
         if decrypt:
-            self.crypto_file = file
+            self.crypto_file = buf
         else:
-            self.plain_file = file
+            self.plain_file = buf
             if key is None:
-                key = self.generate_keys()
+                self.key = self.generate_key()
+        self.generate_subkeys()
 
-    def generate_keys(self):
+    def encrypt(self):
+        file = self.plain_file
+        for i in range(0, len(file), 16):
+            # TODO
+            pass
+
+    def generate_subkeys(self):
+        self.subkeys.append(self.key[0:int(self.key_length / 2)])
+        self.subkeys.append(self.key[int(self.key_length / 2):self.key_length])
+        for i in range(2, 11):
+            xor = int(self.subkeys[i - 1], 16) ^ self.ci[i - 2]
+            xor = hex(xor).replace('0x', '')
+            xor = self.fill_to_full(xor, 16)
+            sub = int(self.subkeys[i - 2], 16) ^ int(self.T(self.P8(xor)), 16)
+            self.subkeys.append(sub)
+
+    def generate_key(self) -> str:
         st = '0123456789abcdef'
         key = ''.join(random.choice(st) for _ in range(self.key_length))
-        self.subkeys.append(key[0:int(self.key_length / 2)])
-        self.subkeys.append(key[int(self.key_length / 2):self.key_length])
         return key
 
-    def P(self, x):
+    def P(self, x) -> str:
         xl = int(x[0], 16)
         xr = int(x[1], 16)
         y = xl ^ self.Fx[xr]
         zr = xr ^ self.Gx[y]
         zl = y ^ self.Fx[zr]
-        return hex(zl).replace('0x','') + hex(zr).replace('0x','')
+        return hex(zl).replace('0x', '') + hex(zr).replace('0x', '')
+
+    def P8(self, x) -> str:
+        x = self.fill_to_full(x, 16)
+        res = ''
+        for i in range(0, 16, 2):
+            res += self.P(x[i:i + 2])
+        return res
+
+    def fill_to_full(self, x, count) -> str:
+        while len(x) < count:
+            x = '0' + x
+        return x
+
+    def T(self, x) -> str:
+        res = ''
+        for i in range(8):
+            for j in range(8):
+                res += x[63 - i - j * 8]
+        return res
 
 
 if __name__ == '__main__':
