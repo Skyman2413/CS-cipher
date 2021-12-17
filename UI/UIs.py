e5a6e452533194d714c8b13519bf846eae0b8108
stepa <stepan26062001@gmail.com>
2021-12-17 04:57:06 +0300

    Исправлена ошибка. Для того чтобы всегда возможно было записать зашифрованный файл, программа теперь записывает и читает в бинарном режиме

--- UI/UIs.py
+++ UI/UIs.py
@@ -75,7 +75,7 @@ class FileDialog(QtWidgets.QDialog, Ui_FileDialog):
         if self.inputFile is None or self.inputFile == '' or self.inputFile == ' ':
             QMessageBox.about(self.FileDialog, "", "Вы забыли выбрать входной файл")
         if self.outputFile is None or self.outputFile == '' or self.outputFile == ' ':
-            self.outputFile = 'output'
+            self.outputFile = 'output.txt'
         if self.generateBox.isChecked():
             self.keyFile = None
         cs = CSCipher(self.inputFile, self.keyFile, self.outputFile, not encrypt)
